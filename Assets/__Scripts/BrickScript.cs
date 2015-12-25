8abde31e5483c508fe19a79cdda14acd595c8e2d
Jennifer Hernández <jhbecares@gmail.com;GIT_AUTHOR_NAME=Jenny Hernndez>
2015-12-25 23:34:52 +0100

    Now it is possible to play a simple brick breaker. Right now, the map is simply hardcoded and we don't have different lives or scores

---
+++ Assets/__Scripts/BrickScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision coll) {
+		Debug.Log ("Destroy on collision");
+		Destroy (gameObject);
+	}
+}
