463734ac0b0c97e312041caaa146a33a9bda52ac
Jennifer Hernández <jhdez.becares@gmail.com;GIT_AUTHOR_NAME=Jenny Hernndez>
2015-12-25 14:03:07 +0100

    First commit

---
+++ Assets/__Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public static float bottomY = -20f;
+
+	void Start() {
+		//Rigidbody.AddForce (0, 1000f, 0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < bottomY) {
+			// Eliminamos la pelota si se nos cae
+			Destroy (this.gameObject);
+
+			// Además, eliminamos uno de los paddles
+			Picker pickerScript = Camera.main.GetComponent<Picker>();
+			pickerScript.BallDestroyed();
+		}
+	}
+}
