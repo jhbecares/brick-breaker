93bb15c1234a7bbf6c562e41fda8f201c42f3a3c
Jennifer Hernández <jhbecares@gmail.com;GIT_AUTHOR_NAME=Jenny Hernndez>
2015-12-25 23:34:52 +0100

    Now it is possible to play a simple brick breaker. Right now, the map is simply hardcoded and we don't have different lives or scores

--- Assets/__Scripts/Picker.cs
+++ Assets/__Scripts/Picker.cs
@@ -5,16 +5,24 @@ using System.Collections.Generic;
 public class Picker : MonoBehaviour {
 
 	public GameObject paddlePrefab;
+	public GameObject ballPrefab;
+
 	public int numBaskets = 3;
 	public float paddleBottomY = -14f;
 	public float paddleSpacingY = 2f;
+	public float ballBottomY = -6f;
 	public List<GameObject> paddleList;
 
 	void Start () {
+		GameObject ballGO = Instantiate ( ballPrefab ) as GameObject;
+		Vector3 pos = Vector3.zero;
+		pos.y = ballBottomY;
+		ballGO.transform.position = pos;
+
 		paddleList = new List<GameObject>();
 		for(int i= 0; i<numBaskets; i++) {
 			GameObject tBasketGO = Instantiate ( paddlePrefab ) as GameObject;
-			Vector3 pos = Vector3.zero;
+			pos = Vector3.zero;
 			pos.y = paddleBottomY + ( paddleSpacingY * i );
 			tBasketGO.transform.position = pos;
 			paddleList.Add(tBasketGO);
