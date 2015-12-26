9fa42dbb3fc6ef862974908d9f38c1a7e7dbe933
Jennifer Hernández <jenny@Mac-de-Jenny.local>
2015-12-26 01:47:25 +0100

    Scores added to the game. Now breaking a brick gives 100 points. Also max scores are set

---
+++ Assets/__Scripts/HighScore.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+
+	static public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Awake() {
+		// If the ApplePickerHighScore already exists, read it
+		if (PlayerPrefs.HasKey ("BrikBreakerHighScore")) {
+			score = PlayerPrefs.GetInt("BrikBreakerHighScore");
+		}
+
+		// Assign the high score to ApplePickerHighScore
+		PlayerPrefs.SetInt ("BrikBreakerHighScore", score);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GUIText gt = this.GetComponent<GUIText> ();
+		gt.text = "High Score: " + score;
+
+		// Update ApplePickerHighScore in PlayerPrefs if necessary
+		if (score > PlayerPrefs.GetInt ("BrikBreakerHighScore")) {
+			PlayerPrefs.SetInt("BrikBreakerHighScore", score);
+		}
+	}
+}
