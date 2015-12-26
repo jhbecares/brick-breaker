6d0b1b27e2aa7ca495d9953f3ce693cc67a9a765
Jennifer Hernández <jhbecares@gmail.com;GIT_AUTHOR_NAME=Jennifer Hernndez>
2015-12-26 01:47:25 +0100

    Scores added to the game. Now breaking a brick gives 100 points. Also max scores are set

--- Assets/__Scripts/PaddleScript.cs
+++ Assets/__Scripts/PaddleScript.cs
@@ -8,19 +8,6 @@ public class PaddleScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		/*// Get the current screen position of the mouse from Input
-		Vector3 mousePos2D = Input.mousePosition;
-
-		// The Camera's z position sets the how far to push the mouse into 3D
-		mousePos2D.z = -Camera.main.transform.position.z;
-
-		// Convert the point from 2D screen space into 3D game world space
-		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
-
-		// Move the x position of this Basket to the x position of the mouse
-		Vector3 pos = this.transform.position;
-		pos.x = mousePos3D.x;
-		this.transform.position = pos;*/
 
 		// Comprobamos si estamos moviendo el paddle a la derecha o a la izquierda
 		// Izquierda
@@ -54,29 +41,9 @@ public class PaddleScript : MonoBehaviour {
 			if (contact.thisCollider == GetComponent<Collider>()) {
 				// Esto corresponde al punto de contacto del paddle
 				float english = contact.point.x - transform.position.x;
-				contact.otherCollider.GetComponent<Rigidbody>().AddForce(200f * english, 0, 0);
+				contact.otherCollider.GetComponent<Rigidbody>().AddForce(300f * english, 0, 0);
 
 			}
 		}
-
-		// Find out what hit this basket
-		/*GameObject collidedWith = coll.gameObject;
-		if (collidedWith.tag == "Ball") {
-			Destroy(collidedWith);
-		}
-
-		// Parse the text of the scoreGT into an int
-		int score = int.Parse (scoreGT.text);
-
-		// Add points for catching the apple
-		score += 100;
-
-		// Convert the score back to a string and display it
-		scoreGT.text = score.ToString ();
-
-		// Track the high score
-		if (score > HighScore.score) {
-			HighScore.score = score;
-		}*/
 	}
 }
