d8f18d5cdbbe9e6a97260774f9119be2a4b9b671
Jennifer Hernández <jhdez.becares@gmail.com;GIT_AUTHOR_NAME=Jenny Hernndez>
2015-12-26 01:47:25 +0100

    Scores added to the game. Now breaking a brick gives 100 points. Also max scores are set

--- Assets/__Scripts/BrickScript.cs
+++ Assets/__Scripts/BrickScript.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class BrickScript : MonoBehaviour {
 
+	public GUIText scoreGT;
+
 	// Use this for initialization
 	void Start () {
-	
+		GameObject scoreGO = GameObject.Find ("ScoreCounter");
+		scoreGT = scoreGO.GetComponent<GUIText> ();
+		scoreGT.text = "0";
 	}
 	
 	// Update is called once per frame
@@ -16,5 +20,27 @@ public class BrickScript : MonoBehaviour {
 	void OnCollisionEnter(Collision coll) {
 		Debug.Log ("Destroy on collision");
 		Destroy (gameObject);
+
+		// Parse the text of the scoreGT into an int
+		int score = int.Parse (scoreGT.text);
+		
+		// Add points for catching the apple
+		score += 100;
+		
+		// Convert the score back to a string and display it
+		scoreGT.text = score.ToString ();
+		
+		// Track the high score
+		if (score > HighScore.score) {
+			HighScore.score = score;
+		}
+
+		// Find out what hit this basket
+		GameObject collidedWith = coll.gameObject;
+		if (collidedWith.tag == "Ball") {
+			Destroy(this);
+		}
+
+
 	}
 }
