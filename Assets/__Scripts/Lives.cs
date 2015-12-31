628f320c4f71cfee508872291c30d62438f450b0
Jennifer Hernández <jhdez.becares@gmail.com;GIT_AUTHOR_NAME=Jenny Hernndez>
2015-12-31 18:14:55 +0100

    Now the game has been modified to include number of lives and to set the score properly even when the game restarts

---
+++ Assets/__Scripts/Lives.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lives : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Awake() {
+		// Assign the high score to ApplePickerHighScore
+		PlayerPrefs.SetInt ("Lives", lives);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GUIText gt = this.GetComponent<GUIText> ();
+		gt.text = "Remaining lives: " + lives;
+	}
+
+	public static int lives {get;set;}
+}
