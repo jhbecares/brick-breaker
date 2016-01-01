6cc2ebcf1edf7f03376efb7050f4d5571d04a7bc
Jennifer Hernández <jhbecares@gmail.com;GIT_AUTHOR_NAME=Jenny Hernndez>
2016-01-01 20:29:03 +0100

    Removed old scene and minor changes

--- Assets/__Scripts/BallScript.cs
+++ Assets/__Scripts/BallScript.cs
@@ -5,7 +5,9 @@ public class BallScript : MonoBehaviour {
 
 	public GameObject ball;
 	public static float bottomY = -20f;
-
+	
+	float maxSpeed = 11f;
+	float minSpeed = 9f;
 
 	void Start() {
 		// Para las pruebas iniciábamos la bola con fuerza, pero ya no lo queremos,
@@ -22,6 +24,17 @@ public class BallScript : MonoBehaviour {
 			// Además, eliminamos uno de los paddles
 			Picker pickerScript = Camera.main.GetComponent<Picker>();
 			pickerScript.BallDestroyed();
+		}
+
+		// Limitamos la velocidad de la bola para que no haga cosas raras,
+		// ya que con un mal golpe se puede acelerar
+		Vector3 vel = this.gameObject.GetComponent<Rigidbody> ().velocity;
+		if (vel.magnitude > maxSpeed) {
+			this.gameObject.GetComponent<Rigidbody> ().velocity = this.gameObject.GetComponent<Rigidbody> ().velocity.normalized * maxSpeed;
+		}
+
+		if (vel.magnitude < minSpeed) {
+			this.gameObject.GetComponent<Rigidbody> ().velocity = this.gameObject.GetComponent<Rigidbody> ().velocity.normalized * minSpeed;
 		}
 	}
 }
