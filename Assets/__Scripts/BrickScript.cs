3563cd206eae982abaf45f2f240551cc4d7337b4
Jenny Hernández <jhdez.becares@gmail.com>
2016-01-01 16:33:12 +0100

    Minion level added

--- Assets/__Scripts/BrickScript.cs
+++ Assets/__Scripts/BrickScript.cs
@@ -22,29 +22,33 @@ public class BrickScript : MonoBehaviour {
 
 	void OnCollisionEnter(Collision coll) {
 		//Debug.Log ("Destroy on collision");
-		Destroy (gameObject);
 
-		// Parse the text of the scoreGT into an int
-		//int score = int.Parse (scoreGT.text);
-		
-		// Add points for catching the apple
-		score += 100;
-		PlayerPrefs.SetInt ("Score", score);
-		
-		// Convert the score back to a string and display it
-		scoreGT.text = "Score: " + score.ToString ();
-		
-		// Track the high score
-		if (score > HighScore.score) {
-			HighScore.score = score;
+		if (this.tag == "Unbreakable") {
+			// No hacemos nada
+		} else {
+			Destroy (gameObject);
+
+			// Parse the text of the scoreGT into an int
+			//int score = int.Parse (scoreGT.text);
+			
+			// Add points for catching the apple
+			score += 100;
+			PlayerPrefs.SetInt ("Score", score);
+			
+			// Convert the score back to a string and display it
+			scoreGT.text = "Score: " + score.ToString ();
+			
+			// Track the high score
+			if (score > HighScore.score) {
+				HighScore.score = score;
+			}
+
+			// Find out what hit this basket
+			GameObject collidedWith = coll.gameObject;
+			if (collidedWith.tag == "Ball") {
+				Destroy (this);
+			}
 		}
-
-		// Find out what hit this basket
-		GameObject collidedWith = coll.gameObject;
-		if (collidedWith.tag == "Ball") {
-			Destroy(this);
-		}
-
 
 	}
 }
