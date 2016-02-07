440b5e430c60d1a3912c544af1a20fc61c29dcea
Jenny Hernández <jhdez.becares@gmail.com>
2016-02-07 12:04:01 +0100

    Added missing powerup explanation in menu scene

--- Assets/__Scripts/MenuScript.cs
+++ Assets/__Scripts/MenuScript.cs
@@ -13,5 +13,13 @@ public class MenuScript : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.S)) {
 			Application.LoadLevel("Level0");
 		}
+		if (Input.GetKeyDown (KeyCode.U)) {
+			AudioListener.pause = false;
+			AudioListener.volume = 1;
+		}
+		if (Input.GetKeyDown(KeyCode.M)) {
+			AudioListener.pause = true;
+			AudioListener.volume = 0;
+		}
 	}
 }
